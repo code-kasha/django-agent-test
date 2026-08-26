6f9bcc97ba67bc5c61b43e2029be05d2c0a117f7
Akash Damle <akash.damle@outlook.com>
2026-08-26 18:10:47 +0530

    gemma4:12b test result

--- inventory/models.py
+++ inventory/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class Item(models.Model):
+    """
+    Represents an item in the inventory.
+    """
+
+    name = models.CharField(max_length=255)
+    sku = models.CharField(max_length=64, unique=True)
+    quantity = models.IntegerField()
--- inventory/views.py
+++ inventory/views.py
@@ -1,1 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Item
+from .serializers import ItemSerializer
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for viewing and editing items.
+    """
+
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
